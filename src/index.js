faba8530d8bd0947fc95ca3dd6e577d996c55901
Adam <aberesdeak@autoscout24.com>
2016-06-24 17:05:27 +0200

    some refactorings + new tets for validation + using sinon + WIP test for targeting

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
     const getAttribute = (el, attr, fallback) => HTMLElement.prototype.getAttribute.call(el, attr) || fallback;
 
-    var loadDoubleClickAPI = _ => {
+    var loadDoubleClickAPI = () => {
         var script = document.createElement('script');
         var s = document.getElementsByTagName('script')[0];
         script.src = 'https://www.googletagservices.com/tag/js/gpt.js';
@@ -24,21 +24,53 @@
         if (window.location.href.indexOf('ads-off') >= 0) { return; }
 
         if (doesScreenResolutionProhibitFillingTheAdSlot(this)) { return; }
-        if (!isAdSlotTypeSupported(this)) { return; }
         if (isUserDealer()) { return; }
 
+        const slotType = getAttribute(this, 'type', 'doubleclick');
+
+        switch(slotType) {
+            case 'doubleclick':
+                loadDoubleClickAdSlot(this);
+                break;
+            default:
+                return;
+        }
+    };
+
+    const loadDoubleClickAdSlot = element => {
         loadDoubleClickAPI();
 
-        const elementId = getAttribute(this, 'element-id', `${Math.random()}`);
-        const slotId = getAttribute(this, 'slot-id');
-        const sizes = JSON.parse(getAttribute(this, 'sizes', '[]'));
-        const sizeMapping = JSON.parse(getAttribute(this, 'size-mapping', '[]'));
+        const elementId = `${Math.random()}`;
+        const slotId = getAttribute(element, 'slot-id');
+        const rawSizes = getAttribute(element, 'sizes');
+        const rawSizeMapping = getAttribute(element, 'size-mapping');
 
-        this.innerHTML = `<div id="${elementId}"></div>`;
+        if (!slotId) { console.warn('Missing attribute: slot-id parameter must be provided.'); return; }
+        if (!rawSizes && !rawSizeMapping) { console.warn('Missing attribute: either sizes or size-mappings must be provided.'); return; }
 
-        this.setAttribute('element-id', elementId);
+        var sizes, sizeMapping;
 
-        googletag.cmd.push(_ => {
+        try {
+            sizes = JSON.parse(rawSizes || '[]');
+            sizeMapping = JSON.parse(rawSizeMapping || '[]');
+        } catch(ex) {
+            console.warn('Invalid attribute: either sizes or size-mappings attribute cannote be JSON-parsed.');
+            return;
+        }
+
+        if (!sizes.length && sizeMapping.length) {
+            sizes = [];
+            sizeMapping.forEach(mapping => {
+                sizes = sizes.concat(mapping[1]);
+            });
+
+            element.setAttribute('sizes', JSON.stringify(sizes));
+        }
+
+        element.innerHTML = `<div id="${elementId}"></div>`;
+
+        googletag.cmd.push(() => {
+            console.log(11111111111111);
             const adslot = googletag.defineSlot(slotId, sizes, elementId);
             adslot.defineSizeMapping(sizeMapping);
             adslot.addService(googletag.pubads());
@@ -60,14 +92,10 @@
         return minX > pageResolution.x || maxX < pageResolution.x || minY > pageResolution.y || maxY < pageResolution.y;
     };
 
-    const isAdSlotTypeSupported = el => {
-        const type = getAttribute(el, 'type', 'doubleclick');
-        return type === 'doubleclick';
-    };
-
     const isUserDealer = () => document.cookie.indexOf('CustomerType=D') > 0;
 
     const setTargeting = pubads => {
+        console.log('TRG');
         const targeting = JSON.parse(document.querySelector('[type="adtargeting/json"]').textContent || '{}');
 
         const matches = location.search.match(/test=([^&]*)/);
@@ -81,11 +109,9 @@
         }
     };
 
-    googletag.cmd.push(_ => {
+    googletag.cmd.push(() => {
         const pubads = window.googletag.pubads();
         setTargeting(pubads);
-
-        // pubads.setForceSafeFrame(true);
         pubads.enableSingleRequest();
         pubads.collapseEmptyDivs(true);
         googletag.enableServices();
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+// const sinon = require('sinon');
 
 const loadScript = () => {
     try {
@@ -118,11 +119,68 @@ describe('When script included on the page', () => {
         after(() => document.cookie = 'User=;expires=0');
 
         it('it should not display any ads', () => {
-            document.body.innerHTML += '<as24-ad-slot type="doubleclick"></as24-ad-slot>';
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="/4467/AS24_MOBILEWEBSITE_DE/detailpage_content2"></as24-ad-slot>';
             const script = document.querySelector('script[src="https://www.googletagservices.com/tag/js/gpt.js"]');
             expect(script).to.be.null;
             expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(0);
         });
     });
 
+    describe('Parameter validation', () => {
+        it('Without slot-id no ad should be loaded', () => {
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" NO_SLOT_ID_HERE></as24-ad-slot>';
+            expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(0);
+        });
+
+        it('Without valid "sizes" or "size-mappings" no ad should be loaded', () => {
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" sizes="a"></as24-ad-slot>';
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" size-mapping="a"></as24-ad-slot>';
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" sizes="a" size-mapping="a"></as24-ad-slot>';
+            expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(0);
+        });
+
+        it('With valid "sizes" ad should be loaded', () => {
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" sizes="[]"></as24-ad-slot>';
+            expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(1);
+        });
+
+        it('With valid "size-mapping" ad should be loaded', () => {
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" size-mapping="[]"></as24-ad-slot>';
+            expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(1);
+        });
+
+        it('With valid "size-mapping" ad should be loaded and sizes attribute should be calculated and set correctly', () => {
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" size-mapping="[[[728,300],[[728,90],[728,300]]],[[0,0],[[300,100],[300,50],[320,50],[320,100]]]]"></as24-ad-slot>';
+            expect(document.querySelectorAll('as24-ad-slot *').length).to.equal(1);
+            expect(document.querySelectorAll('as24-ad-slot[sizes="[[728,90],[728,300],[300,100],[300,50],[320,50],[320,100]]"]').length).to.equal(1);
+        });
+    });
+
+    describe.skip('Targeting', () => {
+        it('When script[type=adtargeting/json] present, targeting params should be set accordingly', (done) => {
+
+            var spy;
+
+            window.googletag = window.googletag || [];
+            window.googletag.cmd = window.googletag.cmd || [];
+            window.googletag.cmd.push(() => {
+                // console.log(window.googletag.pubads().setTargeting.toString());
+                spy = sinon.spy(window.googletag.pubads(), 'setTargeting');
+                // var pa = window.googletag.pubads();
+                // pa.setTargeting('a', 'B');
+                // pa.setTargeting('a', 'B');
+            });
+
+            document.body.innerHTML += '<script type="adtargeting/json">{ "a": 1, "b": 2 }</script>';
+            document.body.innerHTML += '<as24-ad-slot type="doubleclick" slot-id="1" size-mapping="[[[0,0],[[300,100]]]]"></as24-ad-slot>';
+
+            setTimeout(() => {
+                window.googletag.cmd.push(() => {
+                    console.log(spy.callCount);
+                });
+
+                done();
+            }, 1000);
+        });
+    });
 });
