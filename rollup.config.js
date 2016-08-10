2d165f76e9702a03dc9744a0f7adca48d65ecb50
Adam <aberesdeak@autoscout24.com>
2016-08-10 20:20:26 +0000

    using iife for rollup output

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import json from 'rollup-plugin-json';
 export default {
     entry: 'src/index.js',
     // format: 'cjs',
-    format: 'es6',
+    // format: 'es6',
+    format: 'iife',
     plugins: [ json() ],
     dest: 'dist/index2.js'
 };
