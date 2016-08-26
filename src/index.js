5f0006280fa0565624415898884289f500c1787b
Adam <aberesdeak@autoscout24.com>
2016-08-26 11:39:52 +0200

    resetting targeting when as24-ad-targeting element is attached to dom

--- src/index.js
+++ src/index.js
@@ -85,7 +85,6 @@ import { hasAttribute, getAttribute, setAttribute, removeAttribute, loadScript, 
             pubads.collapseEmptyDivs(true);
             pubads.disableInitialLoad();
 
-            setTargeting(pubads);
             googletag.enableServices();
         });
 
@@ -234,23 +233,38 @@ import { hasAttribute, getAttribute, setAttribute, removeAttribute, loadScript, 
             });
         };
 
-        const setTargeting = pubads => {
-            const targetingElements = Array.prototype.slice.call(document.querySelectorAll('as24-ad-targeting'));
-            const targetingObjects = targetingElements.map(el => JSON.parse(el.innerHTML || '{}'));
-            const targeting = {};
-
-            targetingObjects.forEach(obj => Object.assign(targeting, obj));
-
-            for (let key in targeting) {
-                const value = `${targeting[key]}`.split(',');
-                pubads.setTargeting(key, value);
-            }
-        };
-
         try {
             document.registerElement('as24-ad-slot', { prototype });
         } catch(ex) {
             console.warn('Custom element already registered: "as24-ad-slot".');
+        }
+
+        const x = Object.assign(Object.create(HTMLElement.prototype), {
+            attachedCallback() {
+                const targetingElements = Array.from(document.querySelectorAll('as24-ad-targeting'));
+                const targetingObjects = targetingElements.map(el => JSON.parse(el.innerHTML.trim() || '{}'));
+                const targeting = {};
+
+                targetingObjects.forEach(obj => Object.assign(targeting, obj));
+
+                googletag.cmd.push(() => {
+                    const pubads = googletag.pubads();
+                    const oldTargetingKeys = googletag.pubads().getTargetingKeys();
+
+                    oldTargetingKeys.forEach(key => pubads.clearTargeting(key));
+
+                    for (let key in targeting) {
+                        const value = `${targeting[key]}`.split(',');
+                        pubads.setTargeting(key, value);
+                    }
+                });
+            }
+        });
+
+        try {
+            document.registerElement('as24-ad-targeting', { prototype: x });
+        } catch(ex) {
+            console.warn('Custom element already registered: "as24-ad-targeting".');
         }
     }
 
