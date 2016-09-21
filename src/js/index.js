28510185654e2dc52d5811a36d5b11b73804eecc
Adam <aberesdeak@autoscout24.com>
2016-09-21 17:10:30 +0200

    fixed imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import waitUntilAdsCanBeLoaded from './ads-can-be-loaded';
-import AS24AdSlot from './as24-ad-slot';
-import AS24AdTargeting from './as24-ad-targeting';
+
+import registerAdSlotElement from './as24-ad-slot';
+import registerAdTargetingElement from './as24-ad-targeting';
 import { gptinit } from './double-click';
 import { loadScript } from './dom';
 
@@ -8,6 +9,6 @@ waitUntilAdsCanBeLoaded()
     .then(() => {
         loadScript('https://www.googletagservices.com/tag/js/gpt.js');
         gptinit();
-        AS24AdSlot.registerElement('as24-ad-slot');
-        AS24AdTargeting.registerElement('as24-ad-targeting');
+        registerAdSlotElement();
+        registerAdTargetingElement();
     });
