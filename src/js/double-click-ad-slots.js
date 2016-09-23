379dc331164d544f13ed7549c3a16ca42e5e2a1e
Adam <aberesdeak@autoscout24.com>
2016-09-23 11:33:12 +0200

    fixed js error

--- src/js/double-click-ad-slots.js
+++ src/js/double-click-ad-slots.js
@@ -95,12 +95,12 @@ window.addEventListener('transitionend', refreshAdslotsWaitingToBeRefreshed);
 export default register;
 
 export const gptinit = () => {
-    window.googletag.cmd.push(() => {
-        const pubads = window.googletag.pubads();
+    googletag().cmd.push(() => {
+        const pubads = googletag().pubads();
         pubads.enableSingleRequest();
         pubads.collapseEmptyDivs(true);
         pubads.disableInitialLoad();
         // pubads.enableAsyncRendering(); // TODO: check this
-        googletag.enableServices();
+        googletag().enableServices();
     });
 };
