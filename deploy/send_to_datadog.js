bfbffa1b6e89ad018c03beacf7b5d399cd857d7a
Adam <aberesdeak@autoscout24.com>
2016-11-14 10:42:12 +0100

    only sending data to datadog after master builds

--- deploy/send_to_datadog.js
+++ deploy/send_to_datadog.js
@@ -1,14 +1,16 @@
-var fs = require('fs');
-var metrics = require('datadog-metrics');
+if (process.env.CI_BUILD_REF_NAME === 'master') {
+    var fs = require('fs');
+    var metrics = require('datadog-metrics');
 
-function getFilesizeInBytes(filename) {
-    var stats = fs.statSync(filename);
-    var fileSizeInBytes = stats["size"];
-    return fileSizeInBytes
+    function getFilesizeInBytes(filename) {
+        var stats = fs.statSync(filename);
+        var fileSizeInBytes = stats["size"];
+        return fileSizeInBytes
+    }
+
+    var js_bytes = getFilesizeInBytes('dist/index.js');
+
+    metrics.init({ prefix: 'showcar.ads.' });
+    metrics.gauge('js_bytes', js_bytes, ['showcar']);
+    metrics.flush();
 }
-
-var js_bytes = getFilesizeInBytes('dist/index.js');
-
-metrics.init({ prefix: 'showcar.ads.' });
-metrics.gauge('js_bytes', js_bytes, ['showcar']);
-metrics.flush();
