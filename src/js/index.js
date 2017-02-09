0b3b2c3d151d2d37052ed4b006857cc756166782
Adam <aberesdeak@autoscout24.com>
2017-02-09 13:44:42 +0100

    fixed indentation

--- src/js/index.js
+++ src/js/index.js
@@ -22,43 +22,43 @@ waitUntilAdsCanBeLoaded()
 //     return config;
 // })
 .then(() => {
-        const tld = location.hostname.split('.').pop();
-        const useOpenX = tld === 'de' || tld === 'at' || location.hash.indexOf('ads-use-openx') >= 0;
-        const getOpenxUrl = tld => {
-            const urls = {
-                de: 'https://scout24-d.openx.net/w/1.0/jstag?nc=4467-autoscout',
-                at: 'https://scout24-d.openx.net/w/1.0/jstag?nc=4467-autoscout-at'
-            };
-
-            return urls[tld] || urls['de'];
+    const tld = location.hostname.split('.').pop();
+    const useOpenX = tld === 'de' || tld === 'at' || location.hash.indexOf('ads-use-openx') >= 0;
+    const getOpenxUrl = tld => {
+        const urls = {
+            de: 'https://scout24-d.openx.net/w/1.0/jstag?nc=4467-autoscout',
+            at: 'https://scout24-d.openx.net/w/1.0/jstag?nc=4467-autoscout-at'
         };
 
-        if (!useOpenX) {
+        return urls[tld] || urls['de'];
+    };
+
+    if (!useOpenX) {
+        loadScript('https://www.googletagservices.com/tag/js/gpt.js');
+    }
+
+    if (useOpenX) {
+        const convertSizes = sizes => {
+            return JSON.parse(sizes).filter(x => Array.isArray(x) && x.length === 2).map(x => x.join('x'));
+        };
+
+        const activeSlots = Array.from(document.querySelectorAll('as24-ad-slot[sizes]:not([sizes="[]"]):not([out-of-page]):not([immediate]):not([openx-ignore])'));
+
+        window.OX_dfp_ads = activeSlots.map(element => [element.getAttribute('ad-unit'), convertSizes(element.getAttribute('sizes')), element.children[0].id]);
+        loadScript(getOpenxUrl(tld));
+
+        var oxTimeout;
+        const oxCallback = () => {
+            clearTimeout(oxTimeout);
             loadScript('https://www.googletagservices.com/tag/js/gpt.js');
-        }
+        };
 
-        if (useOpenX) {
-            const convertSizes = sizes => {
-                return JSON.parse(sizes).filter(x => Array.isArray(x) && x.length === 2).map(x => x.join('x'));
-            };
-
-            const activeSlots = Array.from(document.querySelectorAll('as24-ad-slot[sizes]:not([sizes="[]"]):not([out-of-page]):not([immediate]):not([openx-ignore])'));
-
-            window.OX_dfp_ads = activeSlots.map(element => [element.getAttribute('ad-unit'), convertSizes(element.getAttribute('sizes')), element.children[0].id]);
-            loadScript(getOpenxUrl(tld));
-
-            var oxTimeout;
-            const oxCallback = () => {
-                clearTimeout(oxTimeout);
-                loadScript('https://www.googletagservices.com/tag/js/gpt.js');
-            };
-
-            oxTimeout = setTimeout(oxCallback, 3000);
-            window.OX_dfp_options = { callback: oxCallback };
-        }
-    })
-    .catch(e => {
-        console.warn(e);
-    });
+        oxTimeout = setTimeout(oxCallback, 3000);
+        window.OX_dfp_options = { callback: oxCallback };
+    }
+})
+.catch(e => {
+    console.warn(e);
+});
 
 window.__temp__test__ads__ = 1;
