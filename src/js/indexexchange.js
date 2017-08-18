3fe19365fc8f2ea7ab5030503e7b051f7ec82568
Pavlo Voznenko <p.voznenko@gmail.com>
2017-08-18 13:46:26 +0200

    AS24-24604: added it, nl, at countries for Index Exchange integration

--- src/js/indexexchange.js
+++ src/js/indexexchange.js
@@ -3,7 +3,8 @@ import { loadScript } from './dom';
 export const loadIndexExchange = () => {
     const tld = location.hostname.split('.').pop();
 
-    if (tld !== 'de') return false;
-
-    loadScript('//js-sec.indexww.com/ht/p/185725-52080520089380.js');
+    if (tld === 'de') loadScript('//js-sec.indexww.com/ht/p/185725-52080520089380.js');
+    else if (tld === 'at') loadScript('//js-sec.indexww.com/ht/p/185725-71871796647060.js');
+    else if (tld === 'it') loadScript('//js-sec.indexww.com/ht/p/185725-124648354720881.js');
+    else if (tld === 'nl') loadScript('//js-sec.indexww.com/ht/p/185725-60876680248506.js');
 };
